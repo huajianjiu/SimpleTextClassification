6651860aa93d25f66eee98b3a9449b091664b2e9
huajianjiu <lastranger8811@gmail.com>
2017-03-03 18:59:42 +0900

    update

--- TextClassification.py
+++ TextClassification.py
@@ -1,13 +1,14 @@
 import numpy as np
 import tensorflow as tf
 import sys
+import os
 
 WORD_VECTOR = {}
 flags = tf.flags
 logging = tf.logging
 flags.DEFINE_string("vector_file", None, "vector_file")
-flags.DEFINE_string("train_data", None, "train_data")
-flags.DEFINE_string("test_data", None, "test_data")
+flags.DEFINE_string("train_data_path", None, "train_data_path")
+flags.DEFINE_string("test_data_path", None, "test_data_path")
 FLAGS = flags.FLAGS
 
 
@@ -21,6 +22,65 @@ class Config(object):
     keep_prob = 0.5
     lr_decay = 0.8
     class_size = 1  # 1bit. 1 for positive, 0 for negative.
+
+class SoftmaxClassifier(object):
+    def __init__(self, is_training=True, config=Config()):
+        self._config = config
+        self.vec_size = vec_size = config.vec_size
+        self.class_size = class_size = config.class_size
+        self._x = tf.placeholder(tf.float32, shape=[vec_size], name="model_x")
+        self._y = tf.placeholder(tf.float32, shape=[class_size], name="model_y")
+        inputs = self._x
+
+        softmax_w = tf.get_variable("softmax_w", [vec_size, class_size])
+        softmax_b = tf.get_variable("softmax_b", [class_size])
+
+        y = tf.matmul(inputs, softmax_w) + softmax_b
+        self.y = tf.nn.softmax(y)
+
+        self._cost = cost = tf.nn.softmax_cross_entropy_with_logits(y, self._y)
+
+        if not is_training:
+            return
+
+        self._lr = tf.Variable(0.0, trainable=False)  # declare lr
+
+        tvars = tf.trainable_variables()
+        grads, _ = tf.clip_by_global_norm(tf.gradients(cost, tvars),
+                                          config.max_grad_norm)
+        optimizer = tf.train.GradientDescentOptimizer(self._lr)
+        self._train_op = optimizer.apply_gradients(zip(grads, tvars))
+
+        def assign_lr(self, session, lr_value):
+            session.run(tf.assign(self._lr, lr_value))
+
+        @property
+        def inputs(self):
+            return self._x
+
+        @property
+        def targets(self):
+            return self._y
+
+        @property
+        def config(self):
+            return self._config
+
+        @property
+        def cost(self):
+            return self._cost
+
+        @property
+        def y(self):
+            return self.y
+
+        @property
+        def lr(self):
+            return self._lr
+
+        @property
+        def train_op(self):
+            return self._train_op
 
 
 def read_vector_file(filename):
@@ -43,17 +103,32 @@ def get_vector(w):
         return np.zeros(200)
 
 
-class SoftmaxClassifier(object):
-    def __init__(self, is_training=True, config=Config()):
-        self._config = config
-        self.vec_size = vec_size = config.vec_size
-        self.class_size = class_size = config.class_size
-        self._x = tf.placeholder(tf.float32, shape=[vec_size], name="model_x")
-        self._y = tf.placeholder(tf.float32, shape=[class_size], name="model_y")
-        inputs = self._x
+def get_text_avg_vector(text):
+    # TODO: get the average vectors of the words in the input text
+    # Be careful with the COMMAS
+    pass
 
-        softmax_w = tf.get_variable("softmax_w", [vec_size, class_size])
-        softmax_b = tf.get_variable("softmax_b", [class_size])
 
-        y = tf.matmul(inputs, softmax_w) + softmax_b
-        self.y = tf.nn.softmax(y)
+def train_review(text, label):
+    input_vector = get_text_avg_vector(text)
+
+
+def train_pn_files(path, label, session):
+    file_list = os.listdir(path)
+    for filename in file_list:
+        with open(filename) as f:
+            text = f.read()
+        train_review(text, label)
+
+def train():
+    if not FLAGS.train_data_path:
+        raise ValueError("Must set --train_data_path")
+    if not FLAGS.test_data_path:
+        raise ValueError("Must set --test_data_path")
+    config = Config()
+    train_data_p = FLAGS.train_data_path + "/pos"
+    train_data_n = FLAGS.train_data_path + "/neg"
+    test_data_p = FLAGS.test_data_path + "/pos"
+    test_data_n = FLAGS.test_data_path + "/neg"
+    # TODO: init session and train then test
+
